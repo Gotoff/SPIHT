2f8cdd9a9243d9c4836bfa466ee3240dbfaa0a65
hawkcoder <hawkcoder@hawkcoderdeMac-mini.local>
2014-05-11 18:37:59 +0800

    Initial Commit

---
+++ SPIHT/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  SPIHT
+//
+//  Created by hawkcoder on 14-5-11.
+//  Copyright (c) 2014年 hawkcoder. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
